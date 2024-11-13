071ef8ecb0f6673113897b543a6a4e70eef7d94e
leeweich <wei-chieh.lee@desy.de>
2024-11-13 16:16:03 +0100

    Finish exercise 1 a)

--- poisson.cc
+++ poisson.cc
@@ -13,8 +13,15 @@ int main() {
 
     ifstream fin("datensumme.txt");
     int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
+    vector<int> zaehler(11);
+    for(int i = 0; i < 234; ++i) {
         fin >> n_i;
+	zaehler[n_i] += 1;
     }
+
+    for(unsigned int k = 0; k < zaehler.size(); ++k) {
+      cout << k << ": " << zaehler[k] << endl;
+    }
+
     fin.close();
 }
